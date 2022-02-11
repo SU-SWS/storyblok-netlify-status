02b0d0199d4132f8d7ce400f6d8241c4dfc09927
Nathan Plowman <nathan.plowman@ffwagency.com>
2022-02-11 11:40:35 -0700

    ADAPT-4162: Adjustments to authentication checking and redirect flow.

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,14 @@
 import React, { useState , useEffect } from 'react'
 import { format, isToday, isYesterday } from 'date-fns'
-import getConfig from 'next/config'
-import { useSession } from "next-auth/react"
+import { useSession, getSession } from "next-auth/react"
 
-export default function Home({ sites }) {
+export default function Home({ sites, session, isAuthorized }) {
   const [selectedSite, setSelectedSite] = useState(null);
-  const [deploys, setDeploys] = useState(null);
-  const { data: session, status } = useSession();
+  const [deploys, setDeploys] = useState(null); 
   
   useEffect(() => {
-    if (window.top == window.self) {
-      window.location.assign('https://app.storyblok.com/oauth/app_redirect');
+    if (window.top === window.self) {
+      window.location.replace('https://app.storyblok.com/oauth/app_redirect');
     }
   }, []);
 
@@ -61,40 +59,57 @@ export default function Home({ sites }) {
     return (hours ? hours + 'h ' : '') + (minutes ? minutes + 'm ' : '') + seconds + 's';
   }
 
-  return (
-    <div className="p-8">
-        <div>
-          <label>Select a site:</label>
-          <select onChange={(e) => setSelectedSite(e.target.value)}>
-            <option value="">-- Select Site --</option>
-            { sites.map(site => <option key={site.id} value={site.id}>{ site.name }</option> )}
-          </select>
-        </div>
-      {
-        deploys && deploys.map((item) => (
-          <div className="flex pb-4 mb-4 border-b border-gray-500 flex justify-between" key={item.id}>
-            <div className="">
-              <div>
-                <strong className="capitalize">{item.context}</strong>: {item.branch}
-                <span className={`align-baseline capitalize duration-200 ml-2 ease-out flex-0-auto inline-block leading-2 px-[4px] py-[2px] relative rounded-sm self-center text-sm transition -top-px whitespace-nowrap font-bold ${stateClasses(item.state)}`}>{item.state}</span>
-              </div>
-              {item.committer && <span>by {item.committer}</span>}
-            </div>
-
-            <div className="text-right">
-              <strong> {day(item.created_at)} at {format(new Date(item.created_at), 'K:mm aa')}</strong>
-              {
-                item.deploy_time && <div>Deployed in: {toHHMMSS(item.deploy_time)}</div>
-              }
-            </div>
+  if (isAuthorized) {
+    return (
+      <div className="p-8">
+          <div>
+            <label>Select a site:</label>
+            <select onChange={(e) => setSelectedSite(e.target.value)}>
+              <option value="">-- Select Site --</option>
+              { sites.map(site => <option key={site.id} value={site.id}>{ site.name }</option> )}
+            </select>
           </div>
-        ))
-      }
-    </div>
+        {
+          deploys && deploys.map((item) => (
+            <div className="flex pb-4 mb-4 border-b border-gray-500 flex justify-between" key={item.id}>
+              <div className="">
+                <div>
+                  <strong className="capitalize">{item.context}</strong>: {item.branch}
+                  <span className={`align-baseline capitalize duration-200 ml-2 ease-out flex-0-auto inline-block leading-2 px-[4px] py-[2px] relative rounded-sm self-center text-sm transition -top-px whitespace-nowrap font-bold ${stateClasses(item.state)}`}>{item.state}</span>
+                </div>
+                {item.committer && <span>by {item.committer}</span>}
+              </div>
+  
+              <div className="text-right">
+                <strong> {day(item.created_at)} at {format(new Date(item.created_at), 'K:mm aa')}</strong>
+                {
+                  item.deploy_time && <div>Deployed in: {toHHMMSS(item.deploy_time)}</div>
+                }
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    )
+  }
+  return (
+    <div>Wait...</div>
   )
 }
 
 export async function getServerSideProps(context) {
+  const referer = context.req.headers.referer || null;
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/api/auth/signin/storyblok',
+        permanent: false,
+      }
+    }
+  }
+  
   const sitesRes = await fetch('https://api.netlify.com/api/v1/sites?filter=all', {
     headers: {
       authorization: `Bearer ${process.env.NETLIFY_TOKEN}`
@@ -110,7 +125,9 @@ export async function getServerSideProps(context) {
 
   return {
     props: {
-      sites
+      sites,
+      session,
+      isAuthorized: session ? true : false
     }
   }
 }
